16b98e6ef9574ccec1500a3d4d2c3cbf8a90d2cd
bot-py-372 <63001902+bot-py-372@users.noreply.github.com>
2021-07-12 01:05:42 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+import countries from 'i18n-iso-countries';
+
+// -countries package to get a countries object in English which has keys as iso codes and values as the country names
+countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
+
+// API KEY AND URL
+const apiKey = process.env.REACT_APP_API_KEY;
+
+
+function App() {
+  // State
+  const [apiData, setApiData] = useState({});
+  const [getState, setGetState] = useState('new york');
+  const [state, setState] = useState('new york');
+
+  
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+
+  // Side effect
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+  }, [apiUrl]);
+
+  const inputHandler = (event) => {
+    setGetState(event.target.value);
+  };
+
+  const submitHandler = () => {
+    setState(getState);
+  };
+
+  const kelvinToFarenheit = (k) => {
+    return (k*1.8 - 459.67).toFixed(2);
+  };
+
+  return (
+    <div className="App">
+      <header className="d-flex justify-content-center align-items-center">
+        <h2>React Weather App</h2>
+      </header>
+      <div className="container">
+        <div className="mt-3 d-flex flex-column justify-content-center align-items-center">
+          <div class="col-auto">
+            <label for="location-name" class="col-form-label">
+              Enter Location :
+            </label>
+          </div>
+          <div class="col-auto">
+            <input
+              type="text"
+              id="location-name"
+              class="form-control"
+              onChange={inputHandler}
+              value={getState}
+            />
+          </div>
+          <button className="btn btn-primary mt-2" onClick={submitHandler}>
+            Search
+          </button>
+        </div>
+
+        <div className="card mt-3 mx-auto" style={{ width: '60vw' }}>
+          {apiData.main ? (
+            <div class="card-body text-center">
+              <img
+                src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
+                alt="weather status icon"
+                className="weather-icon"
+              />
+
+              <p className="h2">
+                {kelvinToFarenheit(apiData.main.temp)}&deg; F
+              </p>
+
+              <p className="h5">
+                <i className="fas fa-map-marker-alt"></i>{' '}
+                <strong>{apiData.name}</strong>
+              </p>
+
+              <div className="row mt-4">
+                <div className="col-md-6">
+                  <p>
+                    <i class="fas fa-temperature-low "></i>{' '}
+                    <strong>
+                      {kelvinToFarenheit(apiData.main.temp_min)}&deg; F
+                    </strong>
+                  </p>
+                  <p>
+                    <i className="fas fa-temperature-high"></i>{' '}
+                    <strong>
+                      {kelvinToFarenheit(apiData.main.temp_max)}&deg; F
+                    </strong>
+                  </p>
+                </div>
+                <div className="col-md-6">
+                  <p>
+                    {' '}
+                    <strong>{apiData.weather[0].main}</strong>
+                  </p>
+                  <p>
+                    <strong>
+                      {' '}
+                      {countries.getName(apiData.sys.country, 'en', {
+                        select: 'official',
+                      })}
+                    </strong>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <h1>Loading</h1>
+          )}
+        </div>
+      </div>
+      <footer className="footer">
+        <code>
+        © 2021: React Weather App
+        </code>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
